3daea2b165eb0e2393ab5a57d32bb07c74d9a8ee
maires_b <baptiste.mairesse@epitech.eu>
2016-03-12 09:51:36 +0100

    autoarch

--- autarch/auto.sh
+++ autarch/auto.sh
@@ -3,7 +3,6 @@ size_swap="500M"
 size_sys="5G"
 size_home="3G"
 
-echo "Attention veuillez faire Attention a ce que votre disque pour arch soit bien sda et non sdb ou sdc ("$>lsblk")"
-read
+read -p "Attention veuillez faire Attention a ce que votre disque pour arch soit bien sda et non sdb ou sdc ("$>lsblk")\n"
 echo "o\nn\np\n\n\n+$size_sys\nn\np\n\n\n+$size_boot\nn\np\n\n\n+$size_swap\nn\np\n\n\n+$size_home\nw\n" > part
 fdisk /dev/sda < part
